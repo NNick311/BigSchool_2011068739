3c6c37a0f3a1523ea0f4562259fa5aa011be91fe
ASUS <ASUS@DESKTOP-BMEVFGG>
2023-03-28 15:38:39 +0700

    page50

--- DinhVoSongThu_2011068739/Models/Category.cs
+++ DinhVoSongThu_2011068739/Models/Category.cs
@@ -11,6 +11,7 @@ namespace DinhVoSongThu_2011068739.Models
         public byte Id { get; set; }
         [Required]
         [StringLength(255)]
+
         public string Name { get; set; }
     }
 }
